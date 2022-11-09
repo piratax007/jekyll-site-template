98e01d22bdbf74bbcb37fc2ef5df4423158ba581
Fausto M. Lagos S <fausto@autonomia.digital>
2022-11-09 08:09:25 -0500

    Uninstall threejs module and modify webpack config to does not use threejs

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,14 @@
 const path = require('path');
 
-// https://webpack.js.org/configuration/
 module.exports = {
   entry: {
-    main: path.join(__dirname, '_webpack', 'main'),
   },
   output: {
-    path: path.resolve(__dirname, 'assets/js'),
-    filename: '[name]-bundle.js',
   },
   resolve: {
-    extensions: ['.json', '.js', '.jsx'],
-    modules: ['node_modules'],
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1, // https://webpack.js.org/loaders/postcss-loader/
-            },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: () => [
-                require('cssnano')(), // https://cssnano.co/
-              ],
-            },
-          },
-          {
-            loader: 'sass-loader',
-            options: {},
-          },
-        ],
-      },
     ],
   },
 };
